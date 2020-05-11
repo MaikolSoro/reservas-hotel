0bbb5c96abc3cb0d8e599e6e3b310d1e664b9dcc
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-05-11 13:06:10 -0600

    Modularizando la plantilla

---
+++ vistas/paginas/modulos/footer.php
@@ -0,0 +1,132 @@
+<!--=====================================
+CONTÁCTENOS
+======================================-->
+
+<div class="contactenos container-fluid bg-white py-4" id="contactenos">
+	
+	<div class="container text-center">
+		
+		<h1 class="py-sm-4">CONTÁCTENOS</h1>
+
+		<form>
+
+			<div class="input-group input-group-lg">
+				
+				<input type="text" class="form-control mb-3 mr-2 form-control-lg" placeholder="Nombre">
+
+				<input type="text" class="form-control mb-3 ml-2 form-control-lg" placeholder="Apellido">
+
+			</div>
+
+			<div class="input-group input-group-lg">
+				
+				<input type="text" class="form-control mb-3 mr-2 form-control-lg" placeholder="Móvil">
+
+				<input type="text" class="form-control mb-3 ml-2 form-control-lg" placeholder="Correo Electrónico">
+
+			</div>
+
+			<textarea class="form-control" rows="6" placeholder="Escribe aquí tu mensaje"></textarea>
+
+			<input type="submit" class="btn btn-success my-4 btn-lg py-3 text-uppercase w-50" value="Enviar">
+
+		</form>
+
+	</div>
+
+</div>
+
+<!--=====================================
+MAPA
+======================================-->
+<div class="mapa container-fluid bg-white p-0">
+	
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23909.813711653725!2d-85.02249575132184!3d9.742711394299597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa1d583087a8411%3A0x4e55ef117a4fdba6!2sBarcel%C3%B3%20Tambor!5e0!3m2!1ses!2scr!4v1576359785641!5m2!1ses!2scr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+	<div class=" p-4 info"> 
+
+		<h3 class="mt-4"><strong>Visítanos</strong></h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+		<p>
+		Apple inc.<br>
+		Infinte Loop.<br>
+		Cupertino, CA 95014<br>
+		408-996-1010
+		</p>
+
+		<p class="pb-4">Email: info@apple.com<br>
+		Tel: 1-800-676-2775</p>
+
+	</div>	
+
+</div>
+
+<!--=====================================
+FOOTER
+======================================-->
+
+<footer class="container-fluid p-0">
+
+	<div class="grid-container">
+			
+		<div class="grid-item d-none d-lg-block pt-2"></div>
+
+		<div class="grid-item d-none d-lg-block pt-2">
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat.</p>
+
+		</div>
+
+		<div class="grid-item pt-2">
+			
+			<ul class="py-1">
+
+				<li>
+					<a href="#" target="_blank"><i class="fab fa-facebook-f lead text-white float-left mx-3"></i></a>
+				</li>
+
+				<li>
+					<a href="#" target="_blank"><i class="fab fa-twitter lead text-white float-left mx-3"></i></a>
+				</li>
+
+				<li>
+					<a href="#" target="_blank"><i class="fab fa-youtube lead text-white float-left mx-3"></i></a>
+				</li>
+
+
+				<li>
+					<a href="#" target="_blank"><i class="fab fa-instagram lead text-white float-left mx-3"></i></a>
+				</li>	
+			
+			</ul>	
+
+		</div>
+
+	</div>
+
+</footer>
+
+<!--=====================================
+REDES SOCIALES MÓVIL
+======================================-->
+
+<ul class="redesMovil p-2 nav nav-justified">
+
+	<li class="nav-item">
+		<a href="#" target="_blank"><i class="fab fa-facebook-f lead text-white"></i></a>
+	</li>
+
+	<li class="nav-item">
+		<a href="#" target="_blank"><i class="fab fa-twitter lead text-white"></i></a>
+	</li>
+
+	<li class="nav-item">
+		<a href="#" target="_blank"><i class="fab fa-youtube lead text-white"></i></a>
+	</li>
+
+	<li class="nav-item">
+		<a href="#" target="_blank"><i class="fab fa-instagram lead text-white"></i></a>
+	</li>	
+
+</ul>	
