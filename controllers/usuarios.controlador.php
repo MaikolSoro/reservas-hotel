3a4277b42a37b25552c4c449bfa6e5a392509e11
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-05-17 19:16:15 -0600

    Encriptar la contraseña

--- controllers/usuarios.controlador.php
+++ controllers/usuarios.controlador.php
@@ -13,13 +13,14 @@ Class ControladorUsuarios{
 			   preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["registroEmail"]) &&
 			    preg_match('/^[a-zA-Z0-9]+$/', $_POST["registroPassword"])){
 
+					$encriptarPassword = crypt($_POST["registroPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
 
 					$encriptarEmail = md5($_POST["registroEmail"]);
 
 					$tabla = "usuarios";
 
 					$datos = array( "nombre" => $_POST["registroNombre"],
-								"password" => $_POST["registroPassword"],
+								"password" => $encriptarPassword,
 								"email"=> $_POST["registroEmail"],
 								"foto" => "",
 								"modo" => "directo",
